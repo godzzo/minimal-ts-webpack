09833575508c49cf70b5c06254e1bb541e3dfd22
Gindilla Péter <godzzo@godzzo.hu>
2022-02-19 10:44:09 +0100

    gitignore added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import * as _ from 'lodash';
+import * as _ from "lodash";
 
 function component() {
-  const element = document.createElement('div');
+	const element = document.createElement("div");
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.innerHTML = _.join(["Hello", "CHANGED webpack"], " ");
 
-  return element;
+	return element;
 }
 
 document.body.appendChild(component());
